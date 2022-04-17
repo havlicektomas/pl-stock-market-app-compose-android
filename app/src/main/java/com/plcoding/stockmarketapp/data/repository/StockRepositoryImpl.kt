8a49b5601148de76689a1b73f5d204b53898d67b
Tomas Havlicek <havlicek.tomas@gmail.com>
2022-04-17 09:30:32 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/repository/StockRepositoryImpl.kt
@@ -0,0 +1,78 @@
+package com.plcoding.stockmarketapp.data.repository
+
+import com.plcoding.stockmarketapp.data.csv.CSVParser
+import com.plcoding.stockmarketapp.data.local.StockDatabase
+import com.plcoding.stockmarketapp.data.mapper.toCompanyListing
+import com.plcoding.stockmarketapp.data.mapper.toCompanyListingEntity
+import com.plcoding.stockmarketapp.data.remote.StockApi
+import com.plcoding.stockmarketapp.domain.model.CompanyListing
+import com.plcoding.stockmarketapp.domain.repository.StockRepository
+import com.plcoding.stockmarketapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class StockRepositoryImpl @Inject constructor(
+    private val api: StockApi,
+    private val db: StockDatabase,
+    private val csvParser: CSVParser<CompanyListing>
+): StockRepository {
+
+    private val stockDao = db.stockDao
+
+    override suspend fun getCompanyListings(
+        fetchFromRemote: Boolean,
+        query: String
+    ): Flow<Resource<List<CompanyListing>>> {
+        return flow {
+            emit(Resource.Loading(true))
+
+            val localListings = stockDao.searchCompanyListings(query)
+            emit(Resource.Success(
+                data = localListings.map {
+                    it.toCompanyListing()
+                }
+            ))
+
+            val isDbEmpty = localListings.isEmpty() && query.isBlank()
+            val shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote
+            if (shouldJustLoadFromCache) {
+                emit(Resource.Loading(false))
+                return@flow
+            }
+
+            val remoteListings = try {
+                val response = api.getListings(query)
+                csvParser.parse(response.byteStream())
+            } catch (e: IOException) {
+                e.printStackTrace()
+                emit(Resource.Error("Couldn't load data"))
+                null
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                emit(Resource.Error("Couldn't load data"))
+                null
+            }
+
+            remoteListings?.let { listings ->
+                stockDao.clearCompanyListings()
+                stockDao.insertCompanyListings(
+                    listings.map {
+                        it.toCompanyListingEntity()
+                    }
+                )
+                emit(Resource.Success(
+                    data = stockDao.searchCompanyListings("").map {
+                        it.toCompanyListing()
+                    }
+                ))
+            }
+
+            emit(Resource.Loading(false))
+        }
+    }
+}
